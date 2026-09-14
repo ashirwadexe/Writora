832acc9c02f5fdf37d8fd998a7b785d636d570bf
ashirwadexe <ashirwadbappy@gmail.com>
2026-09-14 14:20:48 +0530

    addes home routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+
+      </Routes>
+    </>
   )
 }
 
